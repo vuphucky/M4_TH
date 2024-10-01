146a0a7a36e0baef5a57064be95787db7f135a14
kỳ.vũ <phucky1235@gmail.com>
2024-10-01 10:56:03 +0700

    TH2_Springboot

---
+++ TH_spring-boot-exception-handler/src/main/java/com/codegym/th_springbootexceptionhandler/ThSpringBootExceptionHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.th_springbootexceptionhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThSpringBootExceptionHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThSpringBootExceptionHandlerApplication.class, args);
+    }
+
+}
---
+++ TH_spring-boot-exception-handler/src/test/java/com/codegym/th_springbootexceptionhandler/ThSpringBootExceptionHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.th_springbootexceptionhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThSpringBootExceptionHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
