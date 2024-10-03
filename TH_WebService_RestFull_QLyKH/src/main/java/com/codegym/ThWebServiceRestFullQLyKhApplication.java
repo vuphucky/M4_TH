1420515648f341888ced747ebf19080e448d7f9e
kỳ.vũ <phucky1235@gmail.com>
2024-10-03 09:47:17 +0700

    TH-webservice

---
+++ TH_WebService_RestFull_QLyKH/src/main/java/com/codegym/ThWebServiceRestFullQLyKhApplication.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThWebServiceRestFullQLyKhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThWebServiceRestFullQLyKhApplication.class, args);
+    }
+
+}
---
+++ TH_WebService_RestFull_QLyKH/src/main/java/com/codegym/model/Customer.java
@@ -0,0 +1,16 @@
+package com.codegym.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String address;
+}
